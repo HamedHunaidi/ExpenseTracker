ae53d5a9482825a0a6bb3c96a2c3249479581d81
Hamed Hunaidi <hunaidhbh@gmail.com>
2024-03-07 01:32:07 +0300

    set up functions

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -1,5 +1,28 @@
 def main():
-    pass
+    print("Test")
+
+    # Get user input
+    get_user_expense()
+
+    # Write expense to file.
+    save_user_expense()
+
+    # Read file and output expenses
+    summarize_user_expense()
+
+
+def get_user_expense():
+    print("Expense input")
+    expense_name = input("Enter Expense name: ")
+    print(f"Your entered{expense_name}")
+
+
+def save_user_expense():
+    print("Expense saved")
+
+
+def summarize_user_expense():
+    print("Expense output")
 
 
 if __name__ == "__main__":
