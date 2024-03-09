be487382e490b866b090ff543a06ce897e78602e
Hamed Hunaidi <hunaidhbh@gmail.com>
2024-03-09 21:56:25 +0300

    prompts added

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -14,7 +14,24 @@ def main():
 def get_user_expense():
     print("Expense input")
     expense_name = input("Enter Expense name: ")
-    print(f"Your entered{expense_name}")
+    expense_amount = float(input("Enter Expense amount: "))
+
+    print(f"Your entered {expense_name}, {expense_amount}")
+
+    expense_categories = ["Food", "Home", "Work", "Fun", "Misc"]
+
+    while True:
+        print("Select a category: ")
+        for i, category_name in enumerate(expense_categories):
+            print(f"  {i + 1}. {category_name}")
+
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_categories)):
+            
+        print(selected_index)
+        break
 
 
 def save_user_expense():
